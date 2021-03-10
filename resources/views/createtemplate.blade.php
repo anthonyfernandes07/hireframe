9be24e74bb6a25b1d656ba4a91e009a72036c200
anthonyfernandes07 <77669497+anthonyfernandes07@users.noreply.github.com>
2021-03-10 18:27:22 +0000

    Create createtemplate.blade.php

---
+++ resources/views/createtemplate.blade.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Createtemplate</title>
+</head>
+<body>
+    <h1>
+        <form action="">
+            Enter template name: 
+            <input type="text" name="templatename" id="templateid">
+        </form>
+    </h1>
+</body>
+</html>
