6289734f0523c4e261874813a4668961e70912b2
anthonyfernandes07 <77669497+anthonyfernandes07@users.noreply.github.com>
2021-03-11 21:07:41 +0000

    11 march

---
+++ main.js
@@ -0,0 +1,10 @@
+/*
+let headercount;
+headercount= document.getElementById('headercountid');
+while (headercount!=0)
+{
+    Text += "The number is" + headercount;
+    headercount--;
+}
+*/
+alert(Hello);
--- resources/views/createtemplate.blade.php
+++ resources/views/createtemplate.blade.php
@@ -9,10 +9,20 @@
     <h1>
         <form action="{{ route('ctemps') }}" method="POST">
             @csrf
-            Enter template name: 
-            <input type="text" name="templatename" id="templateid">
+            Let's name your template:
+            <input type="text" name="templatename" id="templateid"> 
+            <br>
+            How many sections should we add to the template?
+            <input type="number" name="sectioncount" id="sectioncountid" min="1" max="5">
+            <br>
+            
+            What is the title for section?
+            <input type="text" name="section">
+            <br>
             <button type="submit"> Create template</button>
         </form>
     </h1>
+    <script src="main.js">
+    </script>
 </body>
 </html>
